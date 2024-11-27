cbdcb65461537602c1fa5cf98dc1d937dd747e8f
gus valbuena <gusvalbuena@guss-MacBook-Pro.local>
2024-11-27 18:36:02 -0300

    initial setup: typescript and storybook config

---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+};
