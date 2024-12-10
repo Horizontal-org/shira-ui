9425ed803305510769057a6192f8ea98fad3a1c2
valbuenag <valbuena52@gmail.com>
2024-12-10 14:47:38 -0300

    add the correct typescript and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'shira-ui',
+      formats: ['es', 'cjs'],
       fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ['react'],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React'
+          react: 'React',
+          'react-dom': 'ReactDOM'
         }
       }
     }
